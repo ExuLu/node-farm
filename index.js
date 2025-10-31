128e14e1654c7e48b33e58896aef9a5e016b0ea1
Alena Sharai <exultantislupus@gmail.com>
2025-10-31 13:37:53 +0000

    feat: add new route with json response

--- index.js
+++ index.js
@@ -46,10 +46,20 @@ const server = http.createServer((req, res) => {
     res.end('This is the OVERVIEW');
   } else if (pathName === '/product') {
     res.end('This is a PRODUCT');
+  } else if (pathName === '/api') {
+    fs.readFile(`${__dirname}/dev-data/data.json`, 'utf-8', (err, data) => {
+      const productData = JSON.parse(data);
+      res.writeHead(200, {
+        'Content-type': 'application/json',
+      });
+
+      res.end(data);
+    });
   } else {
     res.writeHead(404, {
       'Content-type': 'text/html',
     });
+
     res.end('<h1>Page not found</h1>');
   }
 });
